8a32353136b5026300c6f4b11cfa90594188230b
jumashafara <jumashafara0@gmail.com>
2023-08-24 10:13:58 +0300

    adjusted base and school templates

--- school/urls.py
+++ school/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'school'
 
 urlpatterns = [
+    path(route='browse', view=views.browse, name='browse')
 ]
--- school/views.py
+++ school/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def browse(request):
+    context = {}
+    template_name = 'school/browse.html'
+    return render(request=request, 
+                  template_name=template_name,
+                   context=context )
